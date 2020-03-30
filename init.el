e0cf91a1e0febf637e7d662b203bb61d697f33f2
ustato <atsuto.fps@gmail.com>
2020-03-30 23:33:20 +0900

    Updated config

--- init.el
+++ init.el
@@ -56,13 +56,13 @@
 
 
 ;; HTTPS 系のリポジトリ
-;; (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;; (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 ;; (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/") t)
 
 ;; HTTP 系のリポジトリ
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+;; (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+;; (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (add-to-list 'package-archives '("ELPA" . "http://tromey.com/elpa/") t)
 
@@ -70,7 +70,7 @@
 ;; (add-to-list 'package-archives  '("marmalade" . "http://marmalade-repo.org/packages/") t)
 
 (package-initialize)
-(straight-normalize-all)
+;; (straight-normalize-all)
 
 
 (use-package markdown-mode)
@@ -102,6 +102,16 @@
 (global-set-key (kbd "<M-up>")    'windmove-up)
 (global-set-key (kbd "<M-right>") 'windmove-right)
 
+(global-set-key (kbd "C-c h")  'windmove-left)
+(global-set-key (kbd "C-c j")  'windmove-down)
+(global-set-key (kbd "C-c k")    'windmove-up)
+(global-set-key (kbd "C-c l") 'windmove-right)
+
+(global-unset-key (kbd "C-<wheel-up>") )
+(global-unset-key (kbd "C-<wheel-down>") )
+(global-unset-key (kbd "C-<triple-wheel-up>") )
+(global-unset-key (kbd "C-<triple-wheel-down>") )
+
 
 ;; use-package
 ;;; company
@@ -121,6 +131,12 @@
 (use-package company-reftex)
 (use-package company-bibtex)
 (use-package company-shell)
+;; (use-package company-jedi)
+
+;;; Docker
+(use-package dockerfile-mode)
+(use-package docker-compose-mode)
+(use-package docker-tramp)
 
 ;;; yatex
 (use-package yatex)                ;; パッケージ読み込み
@@ -169,12 +185,53 @@
                ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
 
 (add-to-list 'org-latex-classes
-             '("report"
+             '("onecolumnsreport"
                "\\documentclass[10.5pt, aJ4]{jarticle}
                [NO-PACKAGES]
                [NO-DEFAULT-PACKAGES]\n
+\\usepackage{ascmac}
+\\usepackage{amsmath}
 \\usepackage{$HOME/.emacs.d/latex/fancyhdr}
-\\usepackage{$HOME/.emacs.d/latex/proceeding}
+\\usepackage{$HOME/.emacs.d/latex/onecolumnsproceeding}
+\\makeatletter
+\\long\\def\\@makecaption#1#2{%
+  \\vskip\\abovecaptionskip  \\iftdir\\sbox\\@tempboxa{#1\\hskip1zw#2}%
+    \\else\\sbox\\@tempboxa{#1~ #2}%
+  \\fi
+  \\ifdim \\wd\\@tempboxa >\\hsize
+    \\iftdir #1\\hskip1zw#2\\relax\\par
+    \\else #1~ #2\\relax\\par\\fi
+  \\else
+  \\global \\@minipagefalse
+  \\hbox to\\hsize{\\hfil\\box\\@tempboxa\\hfil}%
+\\fi
+\\vskip\\belowcaptionskip}
+\\def\\WordCount#1{%
+  \\@tempcnta\\z@
+  \\@tfor \\@tempa:=#1\\do{\\ignorespaces \\advance\\@tempcnta\\@ne}%
+  #1
+  \\\\ \\hrulefill \\\\
+  \\vspace{-8mm}
+  \\begin{flushright}
+    {\\bfseries 文字数： \\the\\@tempcnta 文字}
+  \\end{flushright}
+}
+\\makeatother"
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+               ("\\paragraph{%s}" . "\\paragraph*{%s}")
+               ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+
+(add-to-list 'org-latex-classes
+             '("twocolumnsreport"
+               "\\documentclass[10.5pt, aJ4]{jarticle}
+               [NO-PACKAGES]
+               [NO-DEFAULT-PACKAGES]\n
+\\usepackage{ascmac}
+\\usepackage{amsmath}
+\\usepackage{$HOME/.emacs.d/latex/fancyhdr}
+\\usepackage{$HOME/.emacs.d/latex/twocolumnsproceeding}
 \\makeatletter
 \\long\\def\\@makecaption#1#2{%
   \\vskip\\abovecaptionskip  \\iftdir\\sbox\\@tempboxa{#1\\hskip1zw#2}%
@@ -207,6 +264,7 @@
 
 
 ;;; magit
+(use-package git)
 (use-package magit)
 (global-set-key (kbd "C-x g") 'magit-status)
 
@@ -236,6 +294,62 @@
 ;;;; 既存スニペットを閲覧・編集する
 (define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
 
+;;; jupyter
+(use-package ein)
+(use-package csv-mode)
+(use-package jupyter)
+
+;; julia
+;; SLIME, the Superior Lisp Interaction Mode for Emacs
+
+;;; (use-package slime
+  ;;; :ensure t
+  ;;; :config
+  ;;; (setq slime-net-coding-system 'utf-8-unix)
+  ;;; (setq slime-contribs '(slime-fancy slime-banner slime-media)))
+;;; 
+;;; (global-set-key "\C-cs" 'slime-selector)
+;;; 
+;;; (setq slime-lisp-implementations
+      ;;; '((julia ("julia" "-i") :init julia-slime-init)
+        ;;; (sbcl ("sbcl"))))
+;;; 
+;;; (use-package julia-mode
+  ;;; :ensure t
+  ;;; :config
+  ;;; (add-hook 'julia-mode-hook 'julia-slime-mode)
+  ;;; ;;(add-hook 'julia-mode-hook 'julia-repl-mode)
+  ;;; )
+;;; 
+;;; (defun julia-slime-mode ()
+  ;;; (slime-mode)
+  ;;; (define-key slime-mode-map (kbd "C-x C-e")
+    ;;; 'slime-julia-eval-last-expression))
+;;; 
+;;; (defun slime-julia-eval-last-expression ()
+  ;;; (interactive)
+  ;;; (save-excursion
+    ;;; (let ((end (point)))
+      ;;; (beginning-of-line)
+      ;;; (let ((start (point)))
+	;;; (slime-flash-region start end)
+	;;; (slime-interactive-eval
+	 ;;; (buffer-substring-no-properties start end))))))
+;;; 
+;;; (defun julia-slime ()
+  ;;; (interactive)
+  ;;; (slime 'julia))
+;;; 
+;;; (defvar swank-julia-port 4005
+  ;;; "Port to use for communicating with the Julia swank server.")
+;;; 
+;;; (defun julia-slime-init (file _)
+  ;;; (setq slime-protocol-version 'ignore)
+  ;;; (setq slime-complete-symbol-function 'slime-simple-complete-symbol)
+  ;;; (setq slime-description-autofocus t)
+  ;;; ;;(add-hook 'slime-popup-buffer-mode-hook #'markdown-mode)
+  ;;; (format "using Swank; createserver(port=%d, filename=\"%s\")\n" swank-julia-port file))
+
 
 ;; オリジナルコマンド
 (defun my-find-file-init-el ()
@@ -246,17 +360,17 @@
 
 (defun my-backup-config-to-repository (arg)
   "emacs設定のバックアップコマンド"
-  (interactive "Dbackup repository path (/backup/.emacs.d): ")
+  (interactive "Dbackup repository path (/backup/HOME/.emacs.d): ")
   (straight-freeze-versions)
-  (shell-command (concat "cp ~/.emacs.d/init.el " (format "%S" arg) "init.el"))
-  (shell-command (concat "cp ~/.emacs.d/straight/build-cache.el " (format "%S" arg) "straight/build-cache.el"))
-  (shell-command (concat "cp ~/.spacemacs " (format "%S" arg) "../.spacemacs"))
-  (shell-command (concat "cp ~/.viminfo " (format "%S" arg) "../.viminfo"))
-  (shell-command (concat "cp -rf ~/.emacs.d/straight/versions " (format "%S" arg) "straight/."))
-  (shell-command (concat "cp -rf ~/.emacs.d/mySnippets " (format "%S" arg) "."))
-  (shell-command (concat "cp -rf ~/.emacs.d/latex " (format "%S" arg) "."))
+  (shell-command (concat "cp ~/.emacs.d/init.el " (format "%S" arg) "/init.el"))
+  (shell-command (concat "cp ~/.emacs.d/straight/build-cache.el " (format "%S" arg) "/straight/build-cache.el"))
+  (shell-command (concat "cp ~/.spacemacs " (format "%S" arg) "/../.spacemacs"))
+  (shell-command (concat "cp ~/.viminfo " (format "%S" arg) "/../.viminfo"))
+  (shell-command (concat "cp -rf ~/.emacs.d/straight/versions " (format "%S" arg) "/straight/."))
+  (shell-command (concat "cp -rf ~/.emacs.d/mySnippets " (format "%S" arg) "/."))
+  (shell-command (concat "cp -rf ~/.emacs.d/latex " (format "%S" arg) "/."))
 
-  (shell-command (concat "cd " (format "%S" arg) "../ && git submodule foreach git add -A && git submodule foreach git commit -m \"Updated config\" && git add -A && git commit -m \"Update emacs\" && git push && cd .emacs.d && git push origin HEAD:master"))
+  (shell-command (concat "cd " (format "%S" arg) "../ && git submodule foreach git add -A && git submodule foreach git commit -m \"Updated config\" && git add -A && git commit -m \"Update emacs\" && git push && cd .emacs.d && git push"))
   )
 
 (defun my-file-exists-p (arg)
@@ -271,3 +385,4 @@
   (interactive "Srun command: ")
   (shell-command "%S" arg)
   )
+
